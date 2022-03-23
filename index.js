1a11dcc7aed44efe5b48294efcc7179da18274bb
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-22 22:52:05 -0400

    commit tree a7db948ec626dcbd3e980e7b695360b4a976676b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+// PART 1
+function writeCards(names) {
+    let len = names.length;
+    const greetings = [];
+    for (let i = 0; i < len; i++){
+        greetings.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);    
+    }
+    return greetings;
+}
+
+// PART 2
+function countDown(number) {
+    for (let i = number; i >= 0; i--){
+        console.log(i);
+    }
+}
